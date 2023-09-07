5f1eab04eb0cb8d5390fdc2f50e43a71f04ead30
tanvir214 <tanvirparvez21@gmail.com>
2023-09-07 23:26:13 +0600

    Buy and Sell Stock

---
+++ BuyandSellStock.cpp
@@ -0,0 +1,24 @@
+/*Best Time to Buy and Sell Stock */
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int s=prices.size();
+        int p=0;
+
+        for(int i=0;i<s;i++){
+            for(int j=i+1;j<s;j++){
+                if(prices[j]-prices[i]>p){
+                    //if(p<prices[j]-prices[i]){
+                        p=prices[j]-prices[i];
+                    //}
+                }
+            }
+        }
+        return p;
+    }
+};
